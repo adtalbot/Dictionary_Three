abee7bd07615a61fcbf688a3a654d09c7cbb0619
audrie <audrie.talbot@gmail.com>
2015-06-04 14:49:08 -0700

    Pass spec for clear method for definition class.

--- lib/definition.rb
+++ lib/definition.rb
@@ -12,4 +12,7 @@ class Definition
   define_method(:save) do
     @@definitions.push(self)
   end
+  define_singleton_method(:clear) do
+    @@definitions = []
+  end
 end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -4,6 +4,9 @@ require('word')
 require('definition')
 
 describe(Definition) do
+  before() do
+    Definition.clear()
+  end
   describe('.all') do
     it('is empty at first') do
       expect(Definition.all()).to(eq([]))
@@ -15,5 +18,12 @@ describe(Definition) do
       test_definition.save()
       expect(Definition.all()).to(eq([test_definition]))
     end
-  end    
+  end
+  describe('.clear') do
+    it('clears the array of saved definitions') do
+      Definition.new({:definition_entry => 'furry animal'}).save()
+      Definition.clear()
+      expect(Definition.all()).to(eq([]))
+    end
+  end
 end
