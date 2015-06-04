796324819fc73e99b1c1b6646752ccb3aa80c9a7
audrie talbot & debbie balbo <student@epicodus.com>
2015-06-04 09:59:20 -0700

    Pass specs for save and clear methods.

--- lib/word.rb
+++ lib/word.rb
@@ -3,11 +3,17 @@ class Word
   @@words = []
 
   define_method(:initialize) do |attributes|
-    @name = name
+    @name = attributes.fetch(:name)
     @word_id = @@words.length.+(1)
     @definitions = []
   end
   define_singleton_method(:all) do
+    @@words
+  end
+  define_method(:save) do
+    @@words.push(self)
+  end
+  define_singleton_method(:clear) do
     @@words = []
   end
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -4,8 +4,25 @@ require('word')
 require('definition')
 
 describe(Word) do
+  before() do
+    Word.clear()
+  end
   describe('.all') do
     it('is empty at first') do
+      expect(Word.all()).to(eq([]))
+    end
+  end
+  describe('#save') do
+    it('saves words into the empty array') do
+      test_word = Word.new({:name => "squirrel"})
+      test_word.save
+      expect(Word.all()).to(eq([test_word]))
+    end
+  end
+  describe('.clear') do
+    it('removes all words from the saved array') do
+      Word.new({:name => 'dog'}).save()
+      Word.clear()
       expect(Word.all()).to(eq([]))
     end
   end
