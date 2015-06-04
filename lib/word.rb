37e314d12bf81a04e04e199c3a8a3cb3c12cee9e
audrie talbot & debbie balbo <student@epicodus.com>
2015-06-04 14:06:50 -0700

    Pass spec for definitions and add definition methods.

--- lib/word.rb
+++ lib/word.rb
@@ -1,33 +1,38 @@
 class Word
-  attr_reader(:name, :word_id, :definition)
-  @@words = []
-
-  define_method(:initialize) do |attributes|
-    @name = attributes.fetch(:name)
-    @word_id = attributes.fetch(:word_id)
-    @word_id = @@words.length.+(1)
-    @definitions = []
-  end
-
-  define_singleton_method(:all) do
-    @@words
-  end
-
-  define_method(:save) do
-    @@words.push(self)
-  end
-
-  define_singleton_method(:clear) do
+    attr_reader(:name, :word_id, :definitions)
     @@words = []
-  end
-  
-  define_singleton_method(:find) do |id|
-    found_word = nil
-    @@words.each() do |word|
-      if word.word_id().eql?(id)
-        found_word = word
-      end
+
+    define_method(:initialize) do |attributes|
+      @name = attributes.fetch(:name)
+      @word_id = attributes.fetch(:word_id)
+      @word_id = @@words.length.+(1)
+      @definitions = []
     end
-    found_word
+
+    define_singleton_method(:all) do
+      @@words
+    end
+
+    define_method(:save) do
+      @@words.push(self)
+    end
+
+    define_singleton_method(:clear) do
+      @@words = []
+    end
+
+    define_singleton_method(:find) do |id|
+      found_word = nil
+      @@words.each() do |word|
+        if word.word_id().eql?(id)
+          found_word = word
+        end
+      end
+      found_word
+    end
+
+    define_method(:add_definition) do |new_definition|
+      Definition.new(:definition_entry => new_definition)
+      @definitions.push(new_definition)
+    end
   end
-end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -35,6 +35,24 @@ describe(Word) do
       expect(Word.find(test_word.word_id())).to(eq(test_word))
     end
   end
-  # describe('#add_definition') do
-  #   new_definition = Definition.new
-end
+  describe('#definitions') do
+    it('returns an empty array at first for definitions') do
+      my_word = Word.new({:name => 'squirrel', :word_id => 1})
+      expect(my_word.definitions()).to(eq([]))
+    end
+
+    it('adds a new defintion to a word') do
+      test_word = Word.new({:name => 'squirrel', :word_id => 1})
+      test_word.add_definition("a furry animal")
+      test_word.add_definition("an animal who doesnt get me coffee")
+      expect(test_word.definitions()).to(eq(['a furry animal', 'an animal who doesnt get me coffee']))
+      end
+    end
+    describe('#add_definition') do
+      it('adds a definition to a word') do
+        test_word = Word.new({:name => 'squirrel', :word_id => 1})
+        test_word.add_definition('a furry animal')
+        expect(test_word.definitions()).to(eq(['a furry animal']))
+      end
+    end
+  end
